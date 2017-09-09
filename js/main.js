9bf84a76a741473ca7c6da0fd3a90eea763fc445
sam <sanyochina@qq.com>
2017-09-09 23:58:41 +0800

    fix: 还原误注释

--- js/main.js
+++ js/main.js
@@ -2,17 +2,17 @@ $(document).ready(function() {
 
 
   $('a.blog-button').click(function() {
-    // If already in blog, return early without animate overlay panel again.
-    // if (location.hash && location.hash == "#blog") return;
-    // if ($('.panel-cover').hasClass('panel-cover--collapsed')) return;
-    // $('.main-post-list').removeClass('hidden');
-    // currentWidth = $('.panel-cover').width();
-    // if (currentWidth < 2000) {
-    //   $('.panel-cover').addClass('panel-cover--collapsed');
-    // } else {
-    //   $('.panel-cover').css('max-width',currentWidth);
-    //   $('.panel-cover').animate({'max-width': '320px', 'width': '22%'}, 400, swing = 'swing', function() {} );
-    // }
+    If already in blog, return early without animate overlay panel again.
+    if (location.hash && location.hash == "#blog") return;
+    if ($('.panel-cover').hasClass('panel-cover--collapsed')) return;
+    $('.main-post-list').removeClass('hidden');
+    currentWidth = $('.panel-cover').width();
+    if (currentWidth < 2000) {
+      $('.panel-cover').addClass('panel-cover--collapsed');
+    } else {
+      $('.panel-cover').css('max-width',currentWidth);
+      $('.panel-cover').animate({'max-width': '320px', 'width': '22%'}, 400, swing = 'swing', function() {} );
+    }
 
     
   });
